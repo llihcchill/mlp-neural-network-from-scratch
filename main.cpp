a2c11cffa243e135972f3cdc4dbccd4ad145dea4
llihcchill <125551072+llihcchill@users.noreply.github.com>
2024-06-03 20:04:39 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <string>
 using namespace std;
 
 const int hiddenLayerNodes = 6;
@@ -10,19 +11,35 @@ double hiddenLayerBiases[hiddenLayerNodes];
 double secondLayerWeights[24];
 double outputLayerBiases[4];
 
+double firstCalculations[12];
+double secondCalculations[hiddenLayerNodes];
+double thirdCalculations[24];
+
+double firstOutputCalculations[6];
+double secondOutputCalculations[6];
+double thirdOutputCalculations[6];
+double fourthOutputCalculations[6];
+
+double outputCalculations[4];
+
 // when training is finished, add a function to display all weights and biases on a seperate file
-// inputs will be an array
-// each array will correspond to a node
-// function for making sure that the input array does not exceed the number of nodes
-// function for creating nodes
 // function for backpropogation
 
 void randomInitialisation();
 void hiddenLayerCalculations(int nodes);
+double compareOutput(double outputCalcs[]);
 
 
 int main(int argc, char *argv[]) 
 {
+
+    cout << "What is your first input?" << endl;
+    cout << "Note: it must be either 1 or 0" << endl;
+    cin >> input[0];
+
+    cout << "What is your second input?" << endl;
+    cin >> input[1];
+
     if(input[0] != 1) {
         if(input[0] != 0) {
             cout << "Error: a number besides 1 or 0 has been picked";
@@ -44,7 +61,14 @@ int main(int argc, char *argv[])
     // ------------------------------------------------------------------------------------------------------------
     randomInitialisation();
 
+    // calculate the mathematics behind the hidden layer
     hiddenLayerCalculations(hiddenLayerNodes);
+
+    // compare all four outputs and print the highest one
+    compareOutput(outputCalculations);
+
+    // backpropogation algorithm to 'train' AI by tweaking the weights and biases to get a better result
+    // yet to be made
 
     return 0;
 }
@@ -80,17 +104,6 @@ void randomInitialisation()
 
 void hiddenLayerCalculations(int nodes) 
 {
-    double firstCalculations[12];
-    double secondCalculations[hiddenLayerNodes];
-    double thirdCalculations[24];
-
-    double firstOutputCalculations[6];
-    double secondOutputCalculations[6];
-    double thirdOutputCalculations[6];
-    double fourthOutputCalculations[6];
-
-    double outputCalculations[4];
-    
     // calculating with weights (input x weight)
     for(int i = 0; i < (12 / 2); i++) {
         firstCalculations[i] = input[1] * firstLayerWeights[i];
@@ -123,8 +136,40 @@ void hiddenLayerCalculations(int nodes)
     }
 
     // compare each output to see which one is highest
-
     for(int i = 0; i < 4; i++) {
         outputCalculations[i] = firstOutputCalculations[i] + secondOutputCalculations[i+6] + thirdOutputCalculations[i+12] + fourthOutputCalculations[i+18] + outputLayerBiases[i];
     }
+
+    
+}
+
+double compareOutput(double outputCalcs[]) 
+{
+    int index;
+    double o = 0;
+
+    for(int i = 0; i < 4; i++) {
+        if(outputCalcs[i] > o) {
+            o = outputCalcs[i];
+            index = i;
+        }
+    }
+
+    if(index == 0) {
+        cout << "AI chose the sequence (1, 0)";
+    }
+
+    if(index == 1) {
+        cout << "AI chose the sequence (0, 1)";
+    }
+
+    if(index == 2) {
+        cout << "AI chose the sequence (1, 1)";
+    }
+
+    if(index == 3) {
+        cout << "AI chose the sequence (0, 0)";
+    }
+
+    return o;
 }
